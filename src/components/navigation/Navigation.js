0f894718c86007bb16b7197e356272a26cb8da4e
frannet82 <frannet82@gmail.com>
2018-11-11 17:56:44 -0600

    Adding navigation and cleaning  project

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navigation = () => {
+    return(
+        <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+            <p className='fs f2 b link dim white pa4 pointer'>Sign Out</p>
+        </nav>
+    );
+}
+export default Navigation;
